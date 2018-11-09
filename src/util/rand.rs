01eaa17cd824c3ba28e4ef593e2ada68cc48b65c
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2018-11-10 00:42:41 +0100

    The code is sometimes asking for random values in [0, 0[, fair enough, return 0 in that case.

--- src/util/rand.rs
+++ src/util/rand.rs
@@ -24,27 +24,31 @@ impl Rand {
         self.rng = XorShiftRng::seed_from_u64(seed);
     }
 
-    // TODO
     pub fn gen_f32(&mut self, high: f32) -> f32 {
         let next = self.rng.next_u64();
         next as f32 * high / u64::max_value() as f32
     }
 
-    // TODO
     pub fn gen_signed_f32(&mut self, high: f32) -> f32 {
         let next = self.rng.next_u64();
         next as f32 * high * 2.0 / u64::max_value() as f32 - high
     }
 
-    // TODO
     #[cfg(target_pointer_width = "32")]
     pub fn gen_usize(&mut self, high: usize) -> usize {
-        self.rng.next_u32() as usize % high
+        if high == 0 {
+            0
+        } else {
+            self.rng.next_u32() as usize % high
+        }
     }
 
-    // TODO
     #[cfg(target_pointer_width = "64")]
     pub fn gen_usize(&mut self, high: usize) -> usize {
-        self.rng.next_u64() as usize % high
+        if high == 0 {
+            0
+        } else {
+            self.rng.next_u64() as usize % high
+        }
     }
 }
